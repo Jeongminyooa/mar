b73f093ef6b75549eb459a6d419c52fd356a896f
Jeongminyooa <jmjmjmz732002@naver.com>
2021-02-07 22:42:56 +0900

    Changed App2.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,20 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      name: 'junsoo',
+      name: 'jeongmin',
       posts: []
     }
   }
 
   async componentDidMount(){
-    await API.getPost(1)
-    await API.getPosts()
+    await API.getPost(1) //포스트 아이디에 맞는 값만 반환
+    await API.getPosts() //포스트 값 전부 반환
     await API.modifyPost(1, {title: "new title", content: "new content"})
+    //title에 맞는 포스트 값 수정
     await API.createPost({title: "created post", content: "created content"})
+    //title과 post 생성
     await API.deletePost(3)
+    //아이디에 맞는 값 삭제
     await API.getPosts()
   }
 
@@ -25,7 +28,7 @@ class App extends React.Component {
       <div className="main">
         <div className="post-list"></div>
         <div className="post-body">
-          <p>Title</p>
+          <p>Title2</p>
           <p>Content {this.state.name}</p>
         </div>
       </div>
