c1b005a3e3dd5e2b275e5d7dcf1d384d4d036f59
Jeongminyooa <jmjmjmz732002@naver.com>
2021-02-07 23:50:35 +0900

    c App.js

--- src/components/App.js
+++ src/components/App.js
@@ -28,13 +28,10 @@ class App extends React.Component {
       <div className="main">
         <div className="post-list"></div>
         <div className="post-body">
-<<<<<<< HEAD
-          <p>Title</p>
-          <p>muy name: {this.state.name}</p>
-=======
+
           <p>Title2</p>
           <p>Content {this.state.name}</p>
->>>>>>> Changed App2.js
+
         </div>
       </div>
     );
